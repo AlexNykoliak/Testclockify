c1b2b4af84746e8fdceeb2a88eda1095c9ac2bc0
OLeksandr Nykolyak <nykolyak.o@gmail.com>
2022-10-27 01:13:34 +0300

    add records

--- backend/views.py
+++ backend/views.py
@@ -1,3 +1,18 @@
+import requests
+import json
 from django.shortcuts import render
 
-# Create your views here.
+
+def get_data(request):
+    api_key = "ZTRlZTE0YjctYzQxYy00NDlkLThhYWItZDM0NThiMTgyMjU5"
+    data = {'x-api-key': api_key}
+    url = 'https://api.clockify.me/api/v1/workspaces/6359526f380707416dd956c3/projects/635952e9e7b7d85fb687fa31/tasks'
+    response = requests.get(url, headers=data)
+    response = json.loads(response.text)
+    name = [i['name'] for i in response]
+    return render(request, 'base.html', {'name': name})
+
+
+
+
+
